62b151ad84954949fa29a3555f591d93025a28ca
Pedro Macedo Paronetto Faria Viana <pedro.viana@gavresorts.com.br>
2023-09-15 13:57:11 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Produto
+    {
+        public String Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,24 @@
+﻿using Course;
+using System;
+using System.Globalization;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+        }
+    }
+}
